424d8d2f315ddb989763a4f01327edc7c6998eac
subhash <subhash@subhashs-MacBook-Air.local>
2025-06-30 22:15:06 -0400

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,11 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import ProcessSection from "@/components/ProcessSection";
 
-import StackedCarouselSection from "@/components/StackedCarouselSection";
-
 export default function Home() {
   return (
     <main>
       <Hero />
-      <StackedCarouselSection />
       <ProcessSection />
- 
     </main>
   );
 }
