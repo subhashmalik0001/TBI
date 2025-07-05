d1ca45ab192f08526e24429f551a4ddb859697cc
ashmitsinghk <ashmit.singh.k@gmail.com>
2025-07-05 14:08:58 +0530

    Add Portfolio section and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FounderStories from "@/components/FounderStories";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import ProcessSection from "@/components/ProcessSection";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <div className="max-w-screen-2xl mx-auto px-4">
         <Hero />
         <ProcessSection />
+        <Portfolio />
         <FounderStories />
       </div>
     </main>
