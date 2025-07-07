94010ee8720b3a88ea1fb35ea091d31f3ba04e6c
subhash <subhash@subhashs-MacBook-Air.local>
2025-07-06 21:14:46 -0400

    add

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,9 +1,6 @@
-import ScrollAnimationSection from '@/components/ScrollAnimationSection';
-
 export default function DemoPage() {
   return (
     <div className="min-h-screen">
-      <ScrollAnimationSection />
     </div>
   );
 } 
