3e184048be5df7365b448f6b64bccc105a978208
subhash <subhash@subhashs-MacBook-Air.local>
2025-07-09 15:43:16 -0400

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import("@/components/Navbar"), { ssr: false });
+const Footer = dynamic(() => import("@/components/Footer"), { ssr: false });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
